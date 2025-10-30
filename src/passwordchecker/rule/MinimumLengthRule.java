33bf328ea1557e54def87e5989e1c9fc71cbe646
Swapnil Pal <pal35swapnil35@gmail.com>
2025-10-30 12:56:53 +0000

    Create README.md

--- src/passwordchecker/rule/MinimumLengthRule.java
+++ src/passwordchecker/rule/MinimumLengthRule.java
@@ -7,16 +7,16 @@ import passwordchecker.exception.PasswordValidationException;
  * Validation to check minimum password length
  */
 public class MinimumLengthRule implements ValidationRule {
-    private final int minLength;
+    private final int minLength=8;
 
     /**
      * Constructor a Minimum Length rule
      *
      * @param minLength the minimum required length
      */
-    public MinimumLengthRule(int minLength) {
-        this.minLength = minLength;
-    }
+   // public MinimumLengthRule(int minLength) {
+     //   this.minLength = minLength;
+    //}
 
     /**
      * Validates that password meets minimum length requirement
@@ -25,9 +25,37 @@ public class MinimumLengthRule implements ValidationRule {
      * @throws PasswordValidationException if password is too short
      */
     @Override
-    public void validate(String password) throws PasswordValidationException {
-        if (password == null || password.length() <= minLength) {
+    public  void validate(String password) throws PasswordValidationException {
+        if (password != null && password.length() >= minLength && hasUppercase(password)==true
+                && hasLowercase(password)==true && containsAlteastOneNumber(password)==true){
             throw new PasswordLengthException(minLength);
         }
+    }
+    public  static boolean hasLowercase(String password) {
+        for (char ch : password.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public  static boolean hasUppercase(String password)
+    {
+        for (char ch: password.toCharArray())
+        {
+            if(Character.isUpperCase(ch))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public  static boolean containsAlteastOneNumber(String password) {
+        for (char ch : password.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
